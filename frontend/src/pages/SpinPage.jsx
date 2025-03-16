4f0237c48680e047b056082355ef6369f38f2ded
Mikias Berhanu <bmikias482@gmail.com>
2025-03-16 13:00:20 +0300

    spin page edition

--- frontend/src/pages/SpinPage.jsx
+++ frontend/src/pages/SpinPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const SpinPage = () => {
   return (
-    <div className="flex justify-center items-center bg-blue-500 p-10 min-h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-white border-t-transparent"></div>
+    <div className="flex justify-center items-center p-10">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-sky-400 border-t-transparent"></div>
     </div>
   );
 };
